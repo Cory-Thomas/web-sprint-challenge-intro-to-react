f902c643ae8b493cda722c225b261ccdc3d5ee88
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-08-14 13:08:48 -0400

    Finally got the images/sprites to show up

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
+  const [character, setCharacter] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=9")
+      .then((pokemon) => {
+        // console.log(pokemon["data"]["results"]);
+        setCharacter(pokemon["data"]["results"]);
+      })
+      .catch((error) => {
+        debugger;
+        console.log(`first catch: ${error}`);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      {character.map((char) => (
+        <Character key={char["name"]} url={char["url"]} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,43 @@
-// Write your Character component here
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+export default function Character({ url }) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((poke) => {
+        return poke["data"];
+      })
+      .then((pokeInfo) => {
+        axios
+          .get(pokeInfo["species"]["url"])
+          .then((resp) => {
+            const one = { ...pokeInfo };
+            const two = resp["data"];
+            setData({ ...one, ...two });
+          })
+          .catch((error) => {
+            debugger;
+            console.log(`second catch: ${error}`);
+          });
+      })
+      .catch((error) => {
+        debugger;
+        console.log(`third catch: ${error}`);
+      });
+  }, [url]);
+  console.log(data);
+  return (
+    <section>
+      <h2>{data["name"]}</h2>
+      <div>
+        {data["sprites"] && (
+          <img src={data["sprites"]["front_default"]} alt={`${data["name"]}`} />
+        )}
+      </div>
+    </section>
+  );
+}
